a5e66d9ac64018bbfd44049bedccb3925a41beaa
dhzdhd <ragas5432100@gmail.com>
2022-02-17 15:46:06 +0530

    Add input functionality

--- src/Main.elm
+++ src/Main.elm
@@ -4,9 +4,9 @@ import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput, onClick)
-import Color
 import Material.Icons.Outlined as Outlined
 import Material.Icons.Types exposing (Coloring(..))
+import Array
 
 
 main: Program () Model Msg
@@ -20,48 +20,53 @@ type alias Answer =
   }
 
 type alias Model =
-  { name : String
-  , password : String
-  , passwordAgain : String
-  , answer: Answer
+  { answer: String
   , tries: Int
-  , wordList: List String
+  , wordList: List Answer
   }
+
+
+getICAOCode : String
+getICAOCode =
+  "heh"
 
 
 init : Model
 init =
-  Model "" "" "" (Answer"LOWI" "Blank") 5 (List.repeat 4 "")
+  Model "LOWI" 5 (List.repeat 4 (Answer "" "bg-slate-900"))
 
 
 type Msg
-  = Name String
-  | Password String
-  | PasswordAgain String
-  | Submit
+  = Submit
+  | UpdateList Int String
 
 
-type Color
-  = Blank
-  | Red
-  | Yellow
-  | Green
+getElementByIndex : List Answer -> Int -> String
+getElementByIndex list index =
+  case list |> Array.fromList |> Array.get index of
+    Just res -> res.content
+    Nothing -> ""
+
+
+getColor : Int -> String -> Model -> String
+getColor index content model =
+  if (content == (getElementByIndex model.wordList index)) then "bg-green-500" else "bg-green-500"
 
 
 update : Msg -> Model -> Model
 update msg model =
   case msg of
-    Name name ->
-      { model | name = name }
-
-    Password password ->
-      { model | password = password }
-
-    PasswordAgain password ->
-      { model | passwordAgain = password }
-
     Submit ->
-      model
+      { model
+        | wordList = model.wordList
+        |> List.indexedMap (\index item -> { item | color = (getColor index item.content model) })
+        , tries = if model.wordList |> List.all (\item -> item.color == "bg-green-500") then model.tries else model.tries - 1
+      }
+    UpdateList index content ->
+      { model
+        | wordList = model.wordList
+        |> List.indexedMap (\i item -> if i == index then { item | color = "bg-slate-900", content = content } else item)
+      }
 
 
 view : Model -> Html Msg
@@ -73,27 +78,26 @@ view model =
         , div [ class "absolute bg-black w-20 h-20 hidden" ] [ text "modal" ]
         ]
       , div [ class "flex flex-grow-[1] items-center justify-center flex-col gap-16 text-white" ]
-        [ --(List.repeat (model.tries * 4) (viewBlock "test" Name))
-          div [ class "grid grid-cols-4 gap-5" ] (model.wordList |> List.map (\_ -> viewBlock Name))
+        [ div [ class "grid grid-cols-4 gap-5" ] (model.wordList |> List.indexedMap (\index item -> viewBlock index item.color))
         , button [ onClick Submit, class "bg-slate-900 py-5 px-16 rounded-md hover:shadow-2xl hover:shadow-slate-900 active:shadow-none" ]
             [ text "Submit" ]
         ]
     ]
 
-viewBlock : (String -> msg) -> Html msg
-viewBlock toMsg =
-  input [ maxlength 1, class "bg-slate-900 text-white w-16 h-16 md:w-20 md:h-20 rounded-md text-5xl text-center uppercase" ]
+viewBlock : Int -> String -> Html Msg
+viewBlock index color =
+  input [ onInput (UpdateList index), maxlength 1, class (color ++ " text-white w-16 h-16 md:w-20 md:h-20 rounded-md text-5xl text-center uppercase") ]
     []
 
 
-viewInput : String -> String -> String -> (String -> msg) -> Html msg
-viewInput t p v toMsg =
-  input [ type_ t, placeholder p, value v, onInput toMsg, class "bg-red-900" ] []
+-- viewInput : String -> String -> String -> (String -> msg) -> Html msg
+-- viewInput t p v toMsg =
+--   input [ type_ t, placeholder p, value v, onInput toMsg, class "bg-red-900" ] []
 
 
-viewValidation : Model -> Html msg
-viewValidation model =
-  if model.password == model.passwordAgain then
-    div [ class "" ] [ text "OK" ]
-  else
-    div [ ] [ text "Passwords do not match!" ]
+-- viewValidation : Model -> Html msg
+-- viewValidation model =
+--   if model.password == model.passwordAgain then
+--     div [ class "" ] [ text "OK" ]
+--   else
+--     div [ ] [ text "Passwords do not match!" ]
