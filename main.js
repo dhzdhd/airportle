bae1e14a436c1ab51408aac654f4cd5265865d1d
dhzdhd <ragas5432100@gmail.com>
2022-02-19 12:35:07 +0530

    Fix top level await issue

--- main.js
+++ main.js
@@ -2,19 +2,30 @@ import "./styles.css";
 import { Elm } from "./src/Main.elm";
 
 const airportCodeUrl = "https://pkgstore.datahub.io/core/airport-codes/airport-codes_json/data/9ca22195b4c64a562a0a8be8d133e700/airport-codes_json.json";
-const rawAirportCodes = await (await fetch(airportCodeUrl)).json();
-const airportCodes = rawAirportCodes.filter(element => element["ident"].length == 4 && element["ident"].split('').every(element => isNaN(element)));
-const airport = airportCodes[Math.floor(Math.random() * airportCodes.length)]
-console.log(airport);
 
-const root = document.getElementById("app");
-const app = Elm.Main.init({
-    node: root,
-    flags: {
-        ident: airport.ident,
-        country: airport.iso_country,
-        name: airport.name,
-        continent: airport.continent,
-        aType: airport.type,
-    }
-});
+// ? Top level awaits do not work in vercel :(
+// const rawAirportCodes = await (await fetch(airportCodeUrl)).json();
+
+const fetchAirportInfo = async () => {
+    const rawAirportList = await (await fetch(airportCodeUrl)).json();
+    const airportList = rawAirportList.filter(element => element["ident"].length == 4 && element["ident"].split('').every(element => isNaN(element)));
+    const airport = airportList[Math.floor(Math.random() * airportList.length)]
+
+    console.log(airport);
+
+    return airport;
+};
+
+fetchAirportInfo().then(airport => {
+    const root = document.getElementById("app");
+    const app = Elm.Main.init({
+        node: root,
+        flags: {
+            ident: airport.ident,
+            country: airport.iso_country,
+            name: airport.name,
+            continent: airport.continent,
+            aType: airport.type,
+        }
+    });
+}).catch(err => console.log(err));
