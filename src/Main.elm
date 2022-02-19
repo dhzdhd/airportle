c78937a2fc53408314fac8d0234a1e892e3cc6b5
dhzdhd <ragas5432100@gmail.com>
2022-02-19 20:07:30 +0530

    Add modals and favicon

--- src/Main.elm
+++ src/Main.elm
@@ -22,7 +22,6 @@ type alias Airport =
   , aType: String
   }
 
-
 type alias Answer =
   { content: String
   , color: String
@@ -32,7 +31,7 @@ type alias Model =
   { answer: Airport
   , tries: Int
   , wordList: List Answer
-  , resultModal: Bool
+  , resultModal: WinState
   }
 
 
@@ -44,17 +43,24 @@ getICAOCode =
 
 init : Airport -> (Model, Cmd Msg)
 init airport =
-  ({ answer = airport
-  , tries = 5
-  , wordList = (List.repeat 4 (Answer "" "bg-slate-900"))
-  , resultModal = False
-  }, Cmd.none )
+  ( { answer = airport
+    , tries = 5
+    , wordList = (List.repeat 4 (Answer "" "bg-slate-900"))
+    , resultModal = Neutral
+    }
+  , Cmd.none
+  )
 
 
 type Msg
   = Submit
   | UpdateList Int String
+  | Restart
 
+type WinState
+  = Win
+  | Lose
+  | Neutral
 
 getElementByIndex : List String -> Int -> String
 getElementByIndex list index =
@@ -74,22 +80,40 @@ getColor index content model =
   else "bg-red-500"
 
 
+checkIfWin : Model -> WinState
+checkIfWin model =
+  if (model.wordList |> List.map (\i -> i.content |> String.toLower ) |> String.join "") == (model.answer.ident |> String.toLower) then
+    Win
+  else if model.tries == 0 then
+    Lose
+  else
+    Neutral
+
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
   case msg of
     Submit ->
-      ({ model
-        | wordList = model.wordList
-        |> List.indexedMap (\index item -> { item | color = (getColor index item.content model) })
+      ( { model
+          | wordList = model.wordList
+          |> List.indexedMap (\index item -> { item | color = (getColor index item.content model) })
         , tries = if model.wordList |> List.all (\item -> item.color == "bg-green-500") then model.tries else model.tries - 1
-      }
+        , resultModal = checkIfWin model
+        }
       , Cmd.none
       )
     UpdateList index content ->
-      ({ model
-        | wordList = model.wordList
-        |> List.indexedMap (\i item -> if i == index then { item | color = "bg-slate-900", content = content } else item)
-      }
+      ( { model
+          | wordList = model.wordList
+          |> List.indexedMap (\i item -> if i == index then { item | color = "bg-slate-900", content = content } else item)
+        }
+      , Cmd.none
+      )
+    Restart ->
+      ( { model
+          | tries = 5
+          , wordList = (List.repeat 4 (Answer "" "bg-slate-900"))
+          , resultModal = Neutral
+        }
       , Cmd.none
       )
 
@@ -97,10 +121,10 @@ update msg model =
 view : Model -> Html Msg
 view model =
   div [ class "w-screen min-h-screen flex flex-col items-center gap-5 bg-slate-800" ]
-    [ div [ class "hidden absolute w-screen h-screen flex items-center justify-center" ]
-        [ div [ class "px-96 py-48 bg-slate-900 rounded-md flex flex-col gap-16 text-white text-4xl" ]
-          [ span [ class "text-center" ] [ text "Tries : " ]
-          , button [ class "px-20 py-10 bg-slate-600 rounded-md" ] [ text "You win!" ] ]
+    [ div [ class (showModal model ++ "absolute w-screen h-screen flex items-center justify-center") ]
+        [ div [ class "px-80 py-32 bg-slate-900 rounded-md flex flex-col gap-16 text-white text-4xl" ]
+          [ span [ class "text-center" ] [ text ("Answer: " ++ model.answer.ident) ]
+          , button [ onClick Restart, class "px-16 py-7 bg-slate-600 rounded-md" ] [ text (modalText model) ] ]
         ]
       , div [ class "h-24 w-full px-10 md:px-20 flex flex-row text-4xl justify-between items-center bg-slate-900 text-white" ]
         [ span [ class "" ] [ text "Airportle" ]
@@ -109,14 +133,28 @@ view model =
         ]
       , div [ class "flex flex-grow-[1] items-center justify-center flex-col gap-16 text-white" ]
         [ div [ class "grid grid-cols-4 gap-5" ] (model.wordList |> List.indexedMap (\index item -> viewBlock index item.color))
-        , button [ onClick Submit, class "bg-slate-900 py-5 px-16 rounded-md hover:shadow-2xl hover:shadow-slate-900 active:shadow-none" ]
+        , button [ onClick Submit, class "bg-slate-900 py-5 px-16 rounded-md hover:shadow-2xl text-xl hover:shadow-slate-900 active:shadow-none" ]
             [ text "Submit" ]
         ]
     ]
 
+showModal : Model -> String
+showModal model =
+  case model.resultModal of
+  Neutral -> "hidden "
+  _ -> ""
+
+modalText : Model -> String
+modalText model =
+  case model.resultModal of
+  Win -> "You Won!"
+  Lose -> "You Lost!"
+  Neutral -> ""
+
+
 viewBlock : Int -> String -> Html Msg
 viewBlock index color =
-  input [ onInput (UpdateList index), maxlength 1, class (color ++ " text-white w-16 h-16 md:w-20 md:h-20 rounded-md text-5xl text-center uppercase") ]
+  input [ onInput (UpdateList index),  maxlength 1, class (color ++ " text-white w-16 h-16 md:w-20 md:h-20 rounded-md text-5xl text-center uppercase") ]
     []
 
 subscriptions : Model -> Sub Msg
