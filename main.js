dc63dd0a3a19d8a1123ad093952f9ced6cdeadc6
dhzdhd <ragas5432100@gmail.com>
2022-03-24 19:18:03 +0530

    Again

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const fetchAirportInfo = async () => {
     headers.append('Accept', 'application/json');
 
     const res = await fetch(
-        'http://localhost:5000/api/',
+        'api/',
         { headers: headers }
     );
     const airport = await res.json();
