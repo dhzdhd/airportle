aa8ee49902bf50b2130780022469fe58e6f350cb
dhzdhd <ragas5432100@gmail.com>
2022-03-28 23:28:25 +0530

    Fix padding issues in dialogs

--- src/Main.elm
+++ src/Main.elm
@@ -177,9 +177,9 @@ viewInfoModal model =
           _ -> "Understood"
   in
     div [ class (visibility++ "fixed backdrop-blur-2xl w-screen h-screen flex items-center justify-center") ]
-        [ div [ class "mx-10 px-8 py-10 md:px-40 md:py-24 bg-slate-900 rounded-md flex flex-col gap-8 md:gap-16 text-white text-2xl md:text-3xl max-h-[90%]" ]
+        [ div [ class "mx-10 py-10 px-10 md:px-20 bg-slate-900 rounded-md flex flex-col items-center gap-8 md:gap-16 text-white text-2xl md:text-3xl max-h-[90%]" ]
           [ span [ class "text-center overflow-y-auto whitespace-pre-line" ] [ text contentText ]
-          , button [ onClick buttonFunctionality, class "px-2 py-3 bg-slate-600 rounded-md" ] [ text buttonText] ]
+          , button [ onClick buttonFunctionality, class "w-56 px-2 py-2 bg-slate-600 rounded-md" ] [ text buttonText] ]
         ]
 
 
