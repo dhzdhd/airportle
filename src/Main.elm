abb89847298206ba22813ab392b9fc7b6315c816
dhzdhd <ragas5432100@gmail.com>
2022-04-05 14:15:00 +0530

    Add hover color change to info dialog button

--- src/Main.elm
+++ src/Main.elm
@@ -193,7 +193,7 @@ viewInfoModal model =
     div [ class (visibility++ "fixed backdrop-blur-2xl w-screen h-screen flex items-center justify-center") ]
         [ div [ class "mx-10 py-10 px-10 md:px-20 bg-slate-900 rounded-md flex flex-col items-center gap-8 md:gap-16 text-white text-2xl md:text-3xl max-h-[90%]" ]
           [ span [ class "text-center overflow-y-auto whitespace-pre-line" ] [ text contentText ]
-          , button [ onClick buttonFunctionality, class "w-56 px-2 py-2 bg-slate-600 rounded-md" ] [ text buttonText] ]
+          , button [ onClick buttonFunctionality, class "w-56 px-2 py-2 bg-slate-600 hover:bg-slate-700 rounded-md" ] [ text buttonText] ]
         ]
 
 
